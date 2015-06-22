2eaa5740d08a136341d4c39b64caebd9d7451d58
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-06-22 10:51:29 -0400

    API loading, no ingredients

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ $(document).ready(function() {
     $('button').click(function() {
         $('.recipe').show();
     });
- });
 
-function getRecipeJson() {
-    var apiKey = "ydvxaY3gnXkWQ6W518BR173J2du0ecL6a";
-    var recipeId = 196149;
-    var url = "http://api.bigoven.com/recipe/" + recipeId + "?api_key=" + apiKey;
-    $.ajax({
-        type: "GET",
-        dataType: 'json',
-        cache: false,
-        url: url,
-        success: function(data) {
-            console.log(data);
-            $("#recipeTitle").html(data.Title);
-            $("#instructions").html(data.Instructions);
-        }
-    });
-}
+    function getRecipeJson() {
+        var apiKey = "dvxaY3gnXkWQ6W518BR173J2du0ecL6a";
+        var recipeId = Math.floor(Math.random() * 350000);
+        console.log(recipeId);
+        var url = "http://api.bigoven.com/recipe/" + recipeId + "?api_key=" + apiKey;
+        $.ajax({
+            type: "GET",
+            dataType: 'json',
+            cache: false,
+            url: url,
+            success: function(data) {
+                console.log(data);
+                $("#recipeTitle").html(data.Title);
+                $("#ingredients").html(data.Ingredients[0].Name);
+                $("#instructions").html(data.Instructions);
+            }
+        });
+    }
+    getRecipeJson();
 
-getRecipeJson();
+});

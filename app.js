53a7efa508d444693d64b22b868052c636aa14ea
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-06-26 12:02:14 -0400

    commiting new branch

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 $(document).ready(function() {
     $('button').click(function() {
+        function getRecipeJson() {
+            var apiKey = "dvxaY3gnXkWQ6W518BR173J2du0ecL6a";
+            var recipeId = Math.floor(Math.random() * 350000);
+            var url = "http://api.bigoven.com/recipe/" + recipeId + "?api_key=" + apiKey;
+            $.ajax({
+                type: "GET",
+                dataType: 'json',
+                cache: false,
+                url: url,
+                success: function(data) {
+                    console.log(data);
+                    if (data.StatusCode === 403) {
+                        alert('This recipe is dumb anyways. Refresh and try again');
+                    }
+                    $("#recipeTitle").html(data.Title);
+                    $("#instructions").append(data.Instructions);
+                    $.each(data.Ingredients, function(index) {
+                        $("#ingredients ul").append("<li>" + data.Ingredients[index].Quantity + " " + data.Ingredients[index].Unit + " " + data.Ingredients[index].Name + "</li>");
+                    });
+                }
+            });
+        }
+        getRecipeJson();
         $('.recipe').show();
     });
 
-    function getRecipeJson() {
-        var apiKey = "dvxaY3gnXkWQ6W518BR173J2du0ecL6a";
-        var recipeId = Math.floor(Math.random() * 350000);
-        console.log(recipeId);
-        var url = "http://api.bigoven.com/recipe/" + recipeId + "?api_key=" + apiKey;
-        $.ajax({
-            type: "GET",
-            dataType: 'json',
-            cache: false,
-            url: url,
-            success: function(data) {
-                console.log(data);
-                if (data.StatusCode === 403) {
-                  alert('This recipe is dumb anyways. Refresh and try again');
-                }
-                $("#recipeTitle").html(data.Title);
-                $("#instructions").append(data.Instructions);
-                $.each(data.Ingredients, function(index) {
-                    $("#ingredients ul").append("<li>" + data.Ingredients[index].Quantity + " " + data.Ingredients[index].Unit + " " + data.Ingredients[index].Name + "</li>");
-                    //        $("#ingredients ul").append(data.Ingredients[index].Unit + " ");
-                    //        $("#ingredients ul").append(data.Ingredients[index].Name);
-                    console.log(index);
-                });
-            }
-        });
-    }
-    getRecipeJson();
+
 
 });

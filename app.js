d99f601ddae6919c465c58cdfac4101436b0fc62
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-07-04 15:59:44 -0400

    fixing javascript

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $('button').click(function() {
+           $("#ingredients li").remove();
+        getRecipeJson();
+        $('.recipe').show();
         function getRecipeJson() {
             var apiKey = "dvxaY3gnXkWQ6W518BR173J2du0ecL6a";
             var recipeId = Math.floor(Math.random() * 350000);
@@ -12,18 +15,17 @@ $(document).ready(function() {
                 success: function(data) {
                     console.log(data);
                     if (data.StatusCode === 403) {
-                        alert('This recipe is dumb anyways. Refresh and try again');
+                            getRecipeJson();
+                            return;
                     }
                     $("#recipeTitle").html(data.Title);
-                    $("#instructions").append(data.Instructions);
+                    $("#instructions").html(data.Instructions);
                     $.each(data.Ingredients, function(index) {
                         $("#ingredients ul").append("<li>" + data.Ingredients[index].Quantity + " " + data.Ingredients[index].Unit + " " + data.Ingredients[index].Name + "</li>");
                     });
                 }
             });
         }
-        getRecipeJson();
-        $('.recipe').show();
     });
 
 
